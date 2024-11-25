3e91ae7939a36ae99c7582f332b501aa398736d5
Murilo <muriloj2702@gmail.com>
2024-11-25 20:19:48 -0300

    Criação das functions onSubmit, undoTask, deleteTask no ToDoList.js

--- client/src/ToDoList.js
+++ client/src/ToDoList.js
@@ -1,94 +1,131 @@
-import React, {Component} from "React";
+import React, { Component } from "React";
 import axios from "axios";
-import {Card, Header, Form, Input, Icon} from "semantic-ui-react";
+import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
 
 let endpoint = "http://localhost:9000"
 
-class ToDoList extends Component{
-    constructor(props){
+class ToDoList extends Component {
+    constructor(props) {
         super(props);
 
-        this.state ={
-            task:"",
+        this.state = {
+            task: "",
             items: []
         };
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getTask();
     }
 
-    onChange = (event) =>{
+    onChange = (event) => {
         this.setState({
-            [event.target.name] : event.target.value,
+            [event.target.name]: event.target.value,
         });
     }
 
-    onSubmit
+    onSubmit = () => {
+        let { task } = this.state;
 
-    getTask = () =>{
-        axios.get(endpoint + "/api/tasks").then((res)=>{
-            if(res.data){
+        if (task) {
+            axios.post(endpoint + "/api/task",
+                { task, },
+                {
+                    hearders: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                }
+            ).then((res) =>{
+                this.getTask();
                 this.setState({
-                    items: res.data.map((item)=>{
+                    task:"",
+                });
+                console.log(res);
+            });
+        }
+    };
+
+    getTask = () => {
+        axios.get(endpoint + "/api/tasks").then((res) => {
+            if (res.data) {
+                this.setState({
+                    items: res.data.map((item) => {
                         let color = "yellow";
-                        let style ={
+                        let style = {
                             wordWrap: "break-word"
                         };
 
-                        if(item.status){
+                        if (item.status) {
                             color = "green";
                             style["textDecorationLine"] = "line-through";
                         }
-                        return(
-                            <Card key = {item._id} color={color} fluid className="rough">
+                        return (
+                            <Card key={item._id} color={color} fluid className="rough">
                                 <Card.Content>
                                     <Card.Header textAlign="left">
                                         <div style={style}>{item.task}</div>
                                     </Card.Header>
                                     <Card.Meta textAlign="right">
                                         <Icon
-                                        name="check circle"
-                                        color="blue"
-                                        onClick={() => this.updateTask(item._id)}
+                                            name="check circle"
+                                            color="blue"
+                                            onClick={() => this.updateTask(item._id)}
                                         />
-                                        <span style={{paddingRight: 10}}>Desfazer</span>
+                                        <span style={{ paddingRight: 10 }}>Desfazer</span>
                                         <Icon
-                                        name="delete"
-                                        color="red"
-                                        onClick={()=> this.deleteTask(item._id)}
+                                            name="delete"
+                                            color="red"
+                                            onClick={() => this.deleteTask(item._id)}
                                         />
-                                        <span style={{paddingRight:10}}>Deletar</span>
+                                        <span style={{ paddingRight: 10 }}>Deletar</span>
                                     </Card.Meta>
                                 </Card.Content>
                             </Card>
                         );
                     }),
                 });
-            }else{
+            } else {
                 this.setState({
-                    items:[],
-            });
+                    items: [],
+                });
             }
         });
     };
 
     updateTask = (id) => {
         axios.put(endpoint + "/api/task" + id, {
-            hearders:{
-                "Content-Type":"application/x-www-form-urlencoded"
+            hearders: {
+                "Content-Type": "application/x-www-form-urlencoded"
             },
-        }).then((res)=>{
+        }).then((res) => {
             console.log(res);
             this.getTask();
         });
     }
 
-    undoTask
+    undoTask = (id) => {
+        axios.put(endpoint + "/api/undoTask" + id, {
+            hearders: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+        }).then((res) => {
+            console.log(res);
+            this.getTask();
+        });
+    };
 
-    deleteTask
-    
-    render(){
-        return(
+    deleteTask = (id) => {
+        axios.delete(endpoint + "/api/deleteTask" + id, {
+            hearders: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+        }).then((res) => {
+            console.log(res);
+            this.getTask();
+        });
+    };
+
+    render() {
+        return (
             <div>
                 <div>
                     <Header className="header" as="h2" color="yellow">
@@ -98,17 +135,17 @@ class ToDoList extends Component{
                 <div className="row">
                     <Form onSubmit={this.onSubmit}>
                         <Input
-                        type="text"
-                        name="task"
-                        onChange={this.onChange}
-                        value={this.state.task}
-                        fluid
-                        placeholder="Criar Tarefa"
+                            type="text"
+                            name="task"
+                            onChange={this.onChange}
+                            value={this.state.task}
+                            fluid
+                            placeholder="Criar Tarefa"
                         />
                         {/*<Button> Criar Tarefa</Button>*/}
                     </Form>
                 </div>
-                <div className= "row">
+                <div className="row">
                     <Card.Group>{this.state.items}</Card.Group>
                 </div>
             </div>
