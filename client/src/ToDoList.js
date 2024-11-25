d32d5c5e98db0e89155ab0f16b02a32a7348841f
Murilo <muriloj2702@gmail.com>
2024-11-25 00:08:12 -0300

    Começando a parte do client na ToDoList.js. Adiciona métodos de gerenciamento de tarefas e estrutura de renderização
    - Adicionado o método  para controlar a entrada de texto.
    - Criados os métodos  e  para buscar e atualizar tarefas via API.

--- client/src/ToDoList.js
+++ client/src/ToDoList.js
@@ -16,6 +16,76 @@ class ToDoList extends Component{
     componentDidMount(){
         this.getTask();
     }
+
+    onChange = (event) =>{
+        this.setState({
+            [event.target.name] : event.targe.value,
+        });
+    }
+
+    onSubmit
+
+    getTask = () =>{
+        axios.get(endpoint + "api/tasks").then((res)=>{
+            if(res.data){
+                this.setState({
+                    items: res.data.map((item)=>{
+                        let color = "yellow";
+                        let style ={
+                            wordWrap: "break-word"
+                        };
+
+                        if(item.status){
+                            color = "green";
+                            style["textDecorationLine"] = "line-through";
+                        }
+                        return(
+                            <Card key = {item._id} color={color} fluid className="rough">
+                                <Card.Content>
+                                    <Card.Header textAlign="left">
+                                        <div style={style}>{item.task}</div>
+                                    </Card.Header>
+                                    <Card.Meta textAlign="right">
+                                        <Icon
+                                        name="check circle"
+                                        color="blue"
+                                        onClick={() => this.updateTask(item._id)}
+                                        />
+                                        <span style={{paddingRight: 10}}>Desfazer</span>
+                                        <Icon
+                                        name="delete"
+                                        color="red"
+                                        onClick={()=> this.deleteTask(item._id)}
+                                        />
+                                        <span style={{paddingRight:10}}>Deletar</span>
+                                    </Card.Meta>
+                                </Card.Content>
+                            </Card>
+                        );
+                    }),
+                });
+            }else{
+                this.setState({
+                    items:[],
+            });
+            }
+        });
+    };
+
+    updateTask = (id) => {
+        axios.put(endpoint + "/api/task" + id, {
+            hearders:{
+                "Content-Type":"application/x-www-form-urlencoded"
+            },
+        }).then((res)=>{
+            console.log(res);
+            this.getTask();
+        });
+    }
+
+    undoTask
+
+    deleteTask
     
     render(){
         return(
@@ -25,8 +95,24 @@ class ToDoList extends Component{
                         Lista de Tarefas
                     </Header>
                 </div>
+                <div className="row">
+                    <Form onSubmit={this.onSubmit}>
+                        <Input
+                        type="text"
+                        name="task"
+                        onChange={this.onChange}
+                        value={this.state.task}
+                        fluid
+                        placeholder="Criar Tarefa"
+                        />
+                        {/*<Button> Criar Tarefa</Button>*/}
+                    </Form>
+                </div>
+                <div className= "row">
+                    <Card.Group>{this.state.items}</Card.Group>
+                </div>
             </div>
-        )
+        );
     }
 }
 
